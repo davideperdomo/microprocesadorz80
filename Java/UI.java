7fe52c76656686c99d8f8c9071bcd6fbdab5b2f9
David Perdomo <daeperdomocr@unal.edu.co>
2018-10-23 21:46:33 +0000

    UI

--- Java/UI.java
+++ Java/UI.java
@@ -6,31 +6,11 @@ import java.io.IOException;
 class UI {
   public void init() throws FileNotFoundException, IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    System.out.println("Modo: ");
-    int opcion = Integer.parseInt(br.readLine());
-    switch(opcion){
-      case 1:
-        System.out.println("Cargar programa: ");
-        String archivo = br.readLine();
-        System.out.println(archivo);
-        //ensamblar programa
-        break;
-      case 2:
-        String linea = " ";
-        while(!linea.equals("exit")){
-          System.out.println("Ingrese instrucción : ");
-          linea = br.readLine();
-          System.out.println(linea);
-          //ejecutar instruccion
-        }
-        break;
-      case 3: 
-        System.out.println("Ejecutar programa en memoria: ");
-        String localidad = br.readLine();
-        System.out.println(localidad);
-        break;
-    }
-        
+    System.out.println("MICROPROCESADOR Z80");
+    System.out.println("Cargar programa: ");
+    String archivo = br.readLine();
+    System.out.println(archivo);
+    //ensamblar(archivo);
     br.close();
   }
 }
