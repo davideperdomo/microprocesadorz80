022a91eda957d6232e6cb902b98a11e1b32108c0
Raúl Coral <rfcoralj@unal.edu.co>
2018-12-17 00:58:54 -0500

    Fix opcodes on assembler

--- Assembler.java
+++ Assembler.java
@@ -1,4 +1,435 @@
-public class Assembler {
+import java.io.*;
+import java.util.HashMap;
+import java.util.HashSet;
 
-  
+public class Assembler {
+  private int programSize;
+
+  private HashMap<String, Integer> reg_8bit;
+  private HashMap<String, Integer> reg_16bit;
+  private HashMap<String, Integer> labels;
+  private HashSet<String> opcodes;
+
+  public Assembler() {
+    this.programSize = 0;
+    this.reg_8bit = new HashMap();
+    this.reg_16bit = new HashMap();
+    this.labels = new HashMap();
+    this.opcodes = new HashSet();
+    this.opcodes.add("ADD");
+    this.opcodes.add("SUB");
+    this.opcodes.add("LD");
+    this.opcodes.add("AND");
+    this.opcodes.add("OR");
+    this.opcodes.add("XOR");
+    this.opcodes.add("OUT");
+    this.opcodes.add("JP");
+    this.opcodes.add("INC");
+    this.opcodes.add("DEC");
+    this.opcodes.add("CPL");
+    this.opcodes.add("NEG");
+    this.opcodes.add("CP");
+    this.opcodes.add("RL");
+    this.opcodes.add("RR");
+    this.opcodes.add("BIT");
+    this.opcodes.add("SET");
+    this.opcodes.add("RESET");
+    this.opcodes.add("CALL");
+    this.opcodes.add("HALT");
+    this.opcodes.add("END");
+    this.opcodes.add("ORG");
+    this.opcodes.add("EQU");
+    this.reg_8bit.put("A", 0);
+    this.reg_8bit.put("B", 1);
+    this.reg_8bit.put("C", 2);
+    this.reg_8bit.put("D", 3);
+    this.reg_8bit.put("E", 4);
+    this.reg_8bit.put("H", 5);
+    this.reg_8bit.put("L", 6);
+    this.reg_8bit.put("Ac", 7);
+    this.reg_8bit.put("Bc", 8);
+    this.reg_8bit.put("Cc", 9);
+    this.reg_8bit.put("Dc", 10);
+    this.reg_8bit.put("Ec", 11);
+    this.reg_8bit.put("Hc", 12);
+    this.reg_8bit.put("Lc", 13);
+    this.reg_16bit.put("PC", 0);
+    this.reg_16bit.put("SP", 1);
+    this.reg_16bit.put("IX", 2);
+    this.reg_16bit.put("IY", 3);
+  }
+
+  private int toDec(String address) {
+    return Integer.parseInt(address, 16);
+  }
+
+  private void chargeLabels(String file) throws FileNotFoundException, IOException {
+    String input = null;
+    FileReader f = new FileReader(file);
+    BufferedReader b = new BufferedReader(f);
+    String[] line = null;
+    int auxPointer = 0;
+    while ((input = b.readLine()) != null) {
+      this.programSize++;
+      if (input.charAt(0) == '#') continue;
+      line = input.split(" ");
+      if (line.length == 3 || !this.opcodes.contains(line[0])) {
+        this.labels.put(line[0], auxPointer);
+      }
+      auxPointer+=5;
+    }
+    b.close();
+  }
+
+  public void assemble(String inputFile) throws FileNotFoundException, IOException {
+    this.chargeLabels(inputFile);
+    boolean relocate = false;
+    String line = null, label = null, opcode = null;
+    String[] aux = null, params = null;
+    FileReader f = new FileReader(inputFile);
+    BufferedReader b = new BufferedReader(f);
+    PrintWriter outFile = new PrintWriter("relocatableCode.txt", "UTF-8");
+
+    while((line = b.readLine()) != null) {
+      relocate = false;
+      if (line.charAt(0) == '#') continue;
+      aux = line.split(" ");
+      params = null;
+      label = null;
+
+      // Dividir linea
+      if (aux.length == 3) {
+        label = aux[0];
+        opcode = aux[1];
+        params = aux[2].split(",");
+        relocate = true;
+      } else if (aux.length == 2) {
+        // label opcode
+        if (!this.opcodes.contains(aux[0])) {
+          label = aux[0];
+          opcode = aux[1];
+          relocate = true;
+        // opcode x
+        } else {
+          opcode = aux[0];
+          // opcode label
+          if (this.labels.contains(aux[1])) {
+            label = aux[1];
+            relocate = true;
+          // opcode params
+          } else {
+            params = aux[1].split(",");
+          }
+        }
+      } else {
+        opcode = aux[0];
+      }
+
+      // Inicio
+      switch(opcode) {
+
+        case "ADD":
+          //Reg
+          if (this.reg_8bit.containsKey(params[1])) {
+            this.ir.opcode = 0;
+            this.ir.op1 = this.reg_8bit.get("A");
+            this.ir.op2 = this.reg_8bit.get(params[1]);
+
+          //Mem
+          } else if (params[1].contains("(")) {
+            // direccion indirecto (XX)
+            if (params[1].length() == 4) {
+              this.ir.opcode = 1;
+              this.ir.op1 = this.reg_8bit.get(params[1].substring(1,2));
+              this.ir.op2 = this.reg_8bit.get(params[1].substring(2,3));
+
+            // direccion directo (xxxxH)
+            } else {
+              this.ir.opcode = 2;
+              this.ir.op2 = this.toDec(params[1].substring(1,5));
+            }
+
+          } else {
+            //Num hex XXH
+            this.ir.opcode = 3;
+            this.ir.op1 = this.reg_8bit.get("A");
+            this.ir.op2 = this.toDec(params[1].substring(0, 2));
+          }
+          break;
+
+        case "SUB":
+          //Reg
+          if (this.reg_8bit.containsKey(params[0])) {
+            this.ir.opcode = 4;
+            this.ir.op1 = this.reg_8bit.get("A");
+            this.ir.op2 = this.reg_8bit.get(params[0]);
+
+          //Mem
+          } else if (params[1].contains("(")) {
+            // direccion indirecto (XX)
+            if (params[1].length() == 4) {
+              this.ir.opcode = 5;
+              this.ir.op1 = this.reg_8bit.get(params[0].substring(1,2));
+              this.ir.op2 = this.reg_8bit.get(params[0].substring(2,3));
+
+            // direccion directo (xxxxH)
+            } else {
+              this.ir.opcode = 6;
+              this.ir.op2 = this.toDec(params[0].substring(1,5));
+            }
+
+          } else {
+            //Num hex XXH
+            this.ir.opcode = 7;
+            this.ir.op1 = this.reg_8bit.get("A");
+            this.ir.op2 = this.toDec(params[0].substring(0, 2));
+          }
+          break;
+
+        case "INC":
+          // Reg 8 bits
+          if (this.reg_8bit.containsKey(params[0])) {
+            this.ir.opcode = 8;
+            this.ir.op2 = this.reg_8bit.get(params[0]);
+          // Reg 16 bits
+          } else if (this.reg_16bit.containsKey(params[0])) {
+            this.ir.opcode = 9;
+            this.ir.op2 = this.reg_16bit.get(params[0]);
+          // Mem
+          } else {
+            this.ir.opcode = 10;
+            this.ir.op2 = this.toDec(params[0].substring(1,5));
+          }
+          break;
+
+        case "DEC":
+          // Reg 8 bits
+          if (this.reg_8bit.containsKey(params[0])) {
+            this.ir.opcode = 11;
+            this.ir.op2 = this.reg_8bit.get(params[0]);
+          // Reg 16 bits
+          } else if (this.reg_16bit.containsKey(params[0])) {
+            this.ir.opcode = 12;
+            this.ir.op2 = this.reg_16bit.get(params[0]);
+          // Mem
+          } else {
+            this.ir.opcode = 13;
+            this.ir.op2 = this.toDec(params[0].substring(1,5));
+          }
+          break;
+
+        case "CPL":
+          this.ir.opcode = 14;
+          break;
+
+        case "NEG":
+          this.ir.opcode = 15;
+          break;
+
+        case "LD":
+          // Reg 8 bits
+          if (this.reg_8bit.containsKey(params[0])) {
+            this.ir.op1 = this.reg_8bit.get(params[0]);
+
+            // desde reg
+            if (this.reg_8bit.containsKey(params[1])) {
+              this.ir.opcode 16;
+              this.ir.op2 = this.reg_8bit.get(params[1]);
+
+            // desde mem
+            } else if (params[1].contains("(")) {
+              // memoria indirecto (XX)
+              if (params[1].length() == 4) {
+                this.ir.opcode = 17;
+                this.ir.op2 = this.reg_8bit.get(params[1].substring(1,2));
+                this.ir.op2 <<= 8;
+                this.ir.op2 |= this.reg_8bit.get(params[1].substring(2,3));
+              // memoria directo
+              } else {
+                this.ir.opcode = 18;
+                this.ir.op2 = this.toDec(params[1].substring(1,5));
+              }
+            // desde num
+            } else {
+              this.ir.opcode = 19;
+              this.ir.op2 = this.toDec(params[1].substring(0,2));
+            }
+
+          // Reg 16 bits
+          } else if (this.reg_16bit.containsKey(params[0])) {
+            this.ir.op1 = this.reg_16bit.get(params[0]);
+
+            // desde reg 16 bits
+            if (this.reg_16bit.containsKey(params[1])) {
+              this.ir.opcode = 20;
+              this.ir.op2 = this.reg_16bit.get(params[1]);
+
+            // desde num hex
+            } else if (params[1].length() == 5){
+              this.ir.opcode = 21;
+              this.ir.op2 = this.toDec(params[1].substring(0,4));
+
+            // desde par reg 8 bits
+            } else {
+              this.ir.opcode = 22;
+              this.ir.op2 = this.reg_8bit.get(params[1].substring(0,1));
+              this.ir.op2 <<= 8;
+              this.ir.op2 |= this.reg_8bit.get(params[1].substring(1,2));
+            }
+
+          // Par reg 8 bit desde reg 16 bit
+          } else if (params[0].length() == 2) {
+            this.ir.opcode = 23;
+            this.ir.op1 = this.reg_8bit.get(params[0].substring(0,1));
+            this.ir.op1 <<= 8;
+            this.ir.op1 |= this.reg_8bit.get(params[0].substring(1,2));
+            this.ir.op2 = this.reg_16bit.get(params[1]);
+
+          // Mem
+          } else {
+            // memoria indirecto
+            if (params[0].length() == 4) {
+              this.ir.op1 = this.reg_8bit.get(params[0].substring(0,1));
+              this.ir.op1 <<= 8;
+              this.ir.op1 |= this.reg_8bit.get(params[0].substring(1,2));
+
+              // desde registro
+              if (this.reg_8bit.containsKey(params[1])) {
+                this.ir.opcode = 24;
+                this.it.op2 = this.reg_8bit.get(params[1]);
+              // desde mem
+              } else if (params[1].contains("(")) {
+
+                // desde mem ind
+                if (params[1].length() == 4) {
+                  this.ir.opcode = 25;
+                  this.ir.op2 = this.reg_8bit.get(params[1].substring(0,1));
+                  this.ir.op2 <<= 8;
+                  this.ir.op2 |= this.reg_8bit.get(params[1].substring(1,2));
+
+                // desde mem directa
+                } else {
+                  this.ir.opcode = 26;
+                  this.ir.op2 = this.toDec(params[1].substring(1,5));
+                }
+
+              // desde num
+              } else {
+                this.ir.opcode = 27;
+                this.ir.op2 = this.toDec(params[1].substring(0,2));
+              }
+
+            // memoria directa
+            } else {
+              this.ir.op1 = this.toDec(params[0].substring(1,5));
+
+              // desde registro
+              if (this.reg_8bit.containsKey(params[1])) {
+                this.ir.opcode = 28;
+                this.it.op2 = this.reg_8bit.get(params[1]);
+              // desde mem
+              } else if (params[1].contains("(")) {
+
+                // desde mem ind
+                if (params[1].length() == 4) {
+                  this.ir.opcode = 29;
+                  this.ir.op2 = this.reg_8bit.get(params[1].substring(0,1));
+                  this.ir.op2 <<= 8;
+                  this.ir.op2 |= this.reg_8bit.get(params[1].substring(1,2));
+
+                // desde mem directa
+                } else {
+                  this.ir.opcode = 30;
+                  this.ir.op2 = this.toDec(params[1].substring(1,5));
+                }
+
+              // desde num
+              } else {
+                this.ir.opcode = 31;
+                this.ir.op2 = this.toDec(params[1].substring(0,2));
+              }
+            }
+          }
+          break;
+
+
+      }
+    }
+  }
+
 }
+
+/*
+0  add (with reg) accum
+1  add (with mem ind) accum
+2  add (with mem dir)
+3  add (with num 8 bits) accum
+4  sub (with reg) accum
+5  sub (with mem ind) accum
+6  sub (with mem dir)
+7  sub (with num 8 bits) accum
+8  increment1 (with reg 8 bits)
+9  increment1 (with reg 16 bits)
+10 increment1 (with mem 8 bits)
+11 decrement1 (with reg 8 bits)
+12 decrement1 (with reg 16 bits)
+13 decrement1 (with mem 8 bits)
+14 complement1 (only acc)
+15 complement2 (only acc)
+16 load (with reg 8 - reg 8)
+17 load (with reg 8 - mem ind) A, (HL) ind
+18 load (with reg 8 - mem dir) A, (2016H) dir
+19 load (with reg 8 - num 8)
+20 load (with reg 16 - reg 16)
+21 load (with reg 16 - num )
+22 load (with reg 16 - 2 reg 8 )
+23 load (with 2 reg 8 - reg 16)
+24  load (with mem ind 8 - reg 8)
+25  load (with mem ind 8 - mem ind 8)
+26  load (with mem ind 8 - mem dir 8)
+27  load (with mem ind 8 - num 8)
+28  load (with mem dir 8 - reg 8)
+29  load (with mem dir 8 - mem ind 8)
+30  load (with mem dir 8 - mem dir 8)
+31  load (with mem dir 8 - num 8)
+
+
+32  Input (only acc)
+33  Output (only acc)
+34  AND (with reg 8 bits) acc
+35  AND (with mem ind  8 bits) acc
+36  AND (with mem dir  8 bits) acc
+37  AND (with num 8 bits) acc
+38  OR (with reg 8 bits) acc
+39  OR (with mem ind) acc
+40  OR (with mem dir) acc
+41  OR (with num 8 bits) acc
+42  XOR (with reg 8 bits) acc
+43  XOR (with mem ind bits) acc
+44  XOR (with mem dir bits) acc
+45  XOR (with num 8 bits) acc
+46  comparation (with reg 8 bits) acc
+47  comparation (with num 8 bits) acc
+48  comparation (with mem ind 8 bits) acc
+49  comparation (with mem dir 8 bits) acc
+50  rigthRotation (with reg 8 bits)
+51  rigthRotation (with reg 16 bits)
+52  rigthRotation (with mem ind 8 bits)
+53  rigthRotation (with mem dir 8 bits)
+54  leftRotation (with reg 8 bits)
+55  leftRotation (with reg 16 bits)
+56  leftRotation (with mem ind 8 bits)
+57  leftRotation (with mem dir 8 bits)
+58  checkBit (with reg 8 bits) modify z-flag
+59  checkBit (with mem ind 8 bits) modify z-flag
+60  checkBit (with mem dir 8 bits) modify z-flag
+61  setBit (with reg 8 bits)
+62  setBit (with mem ind 8 bits)
+63  setBit (with mem dir 8 bits)
+64  resetBit (with reg 8 bits)
+65  resetBit (with mem ind 8 bits)
+66  resetBit (with mem dir 8 bits)
+67  jump C (acc > 0)
+68  jump Z (acc == 0)
+69  jump (is a memory address)
+*/
---
+++ Processor.java
@@ -0,0 +1,436 @@
+public class Processor {
+  // 8 bits
+  private final int A = 0, B = 1, C = 2, D = 3, E = 4, H = 5, L = 6;
+  private final int Ac = 7, Bc = 8, Cc = 9, Dc = 10, Ec = 11, Hc = 12, Lc = 13;
+
+  // 16 bits
+  private final int PC = 0, SP = 1, IX = 2, IY = 3;
+
+  // Registros
+  private int[] reg_8bit;
+  private int[] reg_16bit;
+
+  // Banderas
+  private boolean[] flags;
+  private final int carry = 0;
+  private final int sig = 1;
+  private final int zero = 2;
+  private final int pv = 3;
+  private final int aux_carry = 4;
+
+  // Memoria
+  public Memory mem;
+
+  // Instruction register
+  private IR ir;
+  private boolean end;
+
+  public Processor(Memory mem) {
+    this.end = false;
+    this.flags = new boolean[8];
+    this.mem = mem;
+    this.reg_8bit = new int[14];
+    this.reg_16bit =  new int[4];
+    this.ir = new IR();
+    for (int i = 0; i < 14; i++) {
+      this.reg_8bit[i] = 0;
+    }
+    for (int i = 0; i < 4; i++) {
+      this.reg_16bit[i] = 0;
+    }
+    // this.chip = new Chip();
+  }
+
+  public void runProgram() throws IOException {
+    while(!this.end) {
+      this.fetch();
+      this.execute();
+    }
+  }
+
+  public void fetch() {
+    int[] instruction = new int[5];
+    instruction[0] = this.mem[this.reg_16bit[PC]];
+    instruction[1] = this.mem[this.reg_16bit[PC]+1];
+    instruction[2] = this.mem[this.reg_16bit[PC]+2];
+    instruction[3] = this.mem[this.reg_16bit[PC]+3];
+    instruction[4] = this.mem[this.reg_16bit[PC]+4];
+    this.ir.decodeInstruction(instruction);
+    this.reg_16bit[PC] += 5;
+  }
+
+  public void execute() throws IOException{
+    int res = 0;
+    String output = null;
+    switch (this.ir.opcode) {
+      case -1:
+        System.out.println("halt");
+        this.end = true;
+        System.out.println("Fin del programa");
+      break;
+      // add (with reg 8 bits) accum->op1
+      case 0:
+        System.out.println("suma");
+        res = ALU.add(this.reg_8bit[A], this.reg_8bit[this.ir.op2]);
+        this.reg_8bit[A] = res;
+      break;
+
+      // add (with num 8 bits) accum
+      case 1:
+        System.out.println("suma");
+        res = ALU.add(this.reg_8bit[A], this.ir.op2);
+        this.reg_8bit[A] = res;
+      break;
+
+      // add (with mem 8 bits) accum
+      case 2:
+        System.out.println("suma");
+        res = ALU.add(this.reg_8bit[A], this.mem[this.ir.op2]);
+        this.reg_8bit[A] = res;
+      break;
+
+      // sub (with reg 8 bits) accum
+      case 3:
+        System.out.println("resta");
+        res = ALU.sub(this.reg_8bit[A], this.reg_8bit[this.ir.op2]);
+        this.reg_8bit[A] = res;
+      break;
+
+      // sub (with num 8 bits) accum
+      case 4:
+        System.out.println("resta");
+        res = ALU.sub(this.reg_8bit[A], this.ir.op2);
+        this.reg_8bit[A] = res;
+      break;
+
+      // sub (with mem 8 bits) accum
+      case 5:
+        System.out.println("resta");
+        res = ALU.sub(this.reg_8bit[A], this.mem[this.ir.op2]);
+        this.reg_8bit[A] = res;
+      break;
+
+      // 6  increment1 (with reg 8 bits)-> op2
+      case 6:
+        System.out.println("incremento");
+        res = this.reg_8bit[this.ir.op2]++;
+      break;
+      // 7  increment1 (with reg 16 bits)
+      case 7:
+        System.out.println("incremento");
+        res = this.reg_16bit[this.ir.op2]++;
+      break;
+
+      // 8  increment1 (with mem 8 bits)
+      case 8:
+        System.out.println("incremento");
+        res = this.mem[this.ir.op2]++;
+      break;
+
+      // 9  decrement1 (with reg 8 bits)
+      case 9:
+        System.out.println("decremento");
+        res = this.reg_8bit[this.ir.op2]--;
+      break;
+
+      // 10  decrement1 (with reg 16 bits)
+      case 10:
+        System.out.println("decremento");
+        res = this.reg_16bit[this.ir.op2]--;
+      break;
+
+      // 11  decrement1 (with mem 8 bits)
+      case 11:
+        System.out.println("decremento");
+        res = this.mem[this.ir.op2]--;
+      break;
+
+      // 12  complement1 (only acc)
+      case 12:
+        System.out.println("complemento 1");
+        res = ALU.complement1(this.reg_8bit[A]);
+        this.reg_8bit[A] = res;
+      break;
+
+      // 13  complement2 (only acc)
+      case 13:
+        System.out.println("complemento2");
+        res = ALU.complement2(this.reg_8bit[A]);
+        this.reg_8bit[A] = res;
+      break;
+
+      // 14  load (with reg 8 <- reg 8)
+      case 14:
+        System.out.println("load");
+        this.reg_8bit[this.ir.op1] = this.reg_8bit[this.ir.op2];
+      break;
+
+      // 15  load (with reg 16 <- reg 16)
+      case 15:
+        System.out.println("load");
+        this.reg_16bit[this.ir.op1] = this.reg_16bit[this.ir.op2];
+      break;
+
+      // 16  load (with reg 8 <- mem 8)
+      case 16:
+        System.out.println("load");
+        this.reg_8bit[this.ir.op1] = this.mem[this.ir.op2];
+      break;
+
+      // 17  load (with mem 8 <- reg 8)
+      case 17:
+        System.out.println("load");
+        this.mem[this.ir.op1] = this.reg_8bit[this.ir.op2];
+      break;
+
+      // 18  load (with reg 8 <- num 8)
+      case 18:
+        System.out.println("load");
+        this.reg_8bit[this.ir.op1] = this.ir.op2;
+      break;
+
+      // 19  load (with mem 8 <- num 8)
+      case 19:
+        System.out.println("load");
+        this.mem[this.ir.op1] = this.ir.op2;
+      break;
+
+      // 20  load (with mem 8 <- mem 8)
+      case 20:
+        System.out.println("load");
+        this.mem[this.ir.op1] = this.mem[this.ir.op2];
+      break;
+
+      // 21  Input (only acc)
+      case 21:
+        System.out.println("input");
+        this.reg_8bit[A] = this.chip.getDatabus();
+
+      break;
+
+      // 22  Output (only acc)
+      case 22:
+        System.out.println("output");
+        output = Integer.toBinaryString(this.reg_8bit[A]);
+        System.out.print("Bin: ");
+        if (output.length() < 8) {
+          for (int i = 0; i < 8 - output.length(); i++) {
+            System.out.print("0");
+          }
+        } else {
+          output = output.substring(0,8);
+        }
+        this.chip.setDatabus(this.reg_8bit[A]);
+        System.out.print(output + ", Dec: " + this.reg_8bit[A] + "\n");
+      break;
+
+      // 23  AND (with reg 8 bits) acc
+      case 23:
+        System.out.println("and");
+        res = ALU.and(this.reg_8bit[A], this.reg_8bit[this.ir.op2]);
+        this.reg_8bit[A] = res;
+      break;
+
+      // 24  AND (with mem 8 bits) acc
+      case 24:
+        System.out.println("and");
+        res = ALU.and(this.reg_8bit[A], this.mem[this.ir.op2]);
+        this.reg_8bit[A] = res;
+      break;
+
+      // 25  AND (with num 8 bits) acc
+      case 25:
+        System.out.println("and");
+        res = ALU.and(this.reg_8bit[A], this.ir.op2);
+        this.reg_8bit[A] = res;
+      break;
+
+      // 26  OR (with reg 8 bits) acc
+      case 26:
+        System.out.println("or");
+        res = ALU.or(this.reg_8bit[A], this.reg_8bit[this.ir.op2]);
+        this.reg_8bit[A] = res;
+      break;
+
+      // 27  OR (with mem bits) acc
+      case 27:
+        System.out.println("or");
+        res = ALU.or(this.reg_8bit[A], this.mem[this.ir.op2]);
+        this.reg_8bit[A] = res;
+      break;
+
+      // 28  OR (with num 8 bits) acc
+      case 28:
+        System.out.println("or");
+        res = ALU.or(this.reg_8bit[A], this.ir.op2);
+        this.reg_8bit[A] = res;
+      break;
+
+      // 29  XOR (with reg 8 bits) acc
+      case 29:
+        System.out.println("xor");
+        res = ALU.xor(this.reg_8bit[A], this.reg_8bit[this.ir.op2]);
+        this.reg_8bit[A] = res;
+      break;
+
+      // 30  XOR (with mem bits) acc
+      case 30:
+        System.out.println("xor");
+        res = ALU.xor(this.reg_8bit[A], this.mem[this.ir.op2]);
+        this.reg_8bit[A] = res;
+      break;
+
+      // 31  XOR (with num 8 bits) acc
+      case 31:
+        System.out.println("xor");
+        res = ALU.xor(this.reg_8bit[A], this.ir.op2);
+        this.reg_8bit[A] = res;
+      break;
+
+      // 32  comparation (with reg 8 bits) acc
+      case 32:
+        System.out.println("comparacion");
+        if (ALU.equal(this.reg_8bit[A], this.reg_8bit[this.ir.op2])) {
+          // TODO Banderas
+          this.flags[this.carry]=false;
+          this.flags[this.zero]=true;
+        }else if(ALU.lessThan(this.reg_8bit[A], this.reg_8bit[this.ir.op2])) {
+          // TODO Banderas
+          this.flags[this.carry]=true;
+          this.flags[this.zero]=false;
+        } else {
+          // TODO Banderas
+          this.flags[this.carry]=false;
+          this.flags[this.zero]=true;
+        }
+      break;
+
+      // 33  comparation (with num 8 bits) acc
+      case 33:
+        System.out.println("comparacion");
+        if (ALU.equal(this.reg_8bit[A], this.ir.op2)) {
+          // TODO Banderas
+          this.flags[this.carry]=false;
+          this.flags[this.zero]=true;
+        }else if(ALU.lessThan(this.reg_8bit[A], this.ir.op2)) {
+          // TODO Banderas
+          this.flags[this.carry]=true;
+          this.flags[this.zero]=false;
+        } else {
+          // TODO Banderas
+          this.flags[this.carry]=false;
+          this.flags[this.zero]=true;
+        }
+      break;
+
+      // 34  comparation (with mem 8 bits) acc
+      case 34:
+        System.out.println("comparacion");
+        if (ALU.equal(this.reg_8bit[A], this.mem[this.ir.op2])) {
+          // TODO Banderas
+          this.flags[this.carry]=false;
+          this.flags[this.zero]=true;
+        }else if(ALU.lessThan(this.reg_8bit[A], this.mem[this.ir.op2])) {
+          // TODO Banderas
+          this.flags[this.carry]=true;
+          this.flags[this.zero]=false;
+        } else {
+          // TODO Banderas
+          this.flags[this.carry]=false;
+          this.flags[this.zero]=true;
+        }
+      break;
+
+      // 35  rigthRotation (with reg 8 bits)
+      case 35:
+        System.out.println("rotacion derecha");
+        this.reg_8bit[this.ir.op2] = ALU.rigthRotation8(this.reg_8bit[this.ir.op2]);
+      break;
+
+      // 36  rigthRotation (with reg 16 bits)
+      case 36:
+        System.out.println("rotacion derecha");
+        this.reg_16bit[this.ir.op2] = ALU.rigthRotation16(this.reg_16bit[this.ir.op2]);
+      break;
+
+      // 37  rigthRotation (with mem 8 bits)
+      case 37:
+        System.out.println("rotacion derecha");
+        this.mem[this.ir.op2] = ALU.rigthRotation8(this.mem[this.ir.op2]);
+      break;
+
+      // 38  leftRotation (with reg 8 bits)
+      case 38:
+        System.out.println("rotacion izquierda");
+        this.reg_8bit[this.ir.op2] = ALU.leftRotation8(this.reg_8bit[this.ir.op2]);
+      break;
+
+      // 39  leftRotation (with reg 16 bits)
+      case 39:
+        System.out.println("rotacion izquierda");
+        this.reg_16bit[this.ir.op2] = ALU.leftRotation16(this.reg_16bit[this.ir.op2]);
+      break;
+
+      // 40  leftRotation (with mem 8 bits)
+      case 40:
+        System.out.println("rotacion izquierda");
+        this.mem[this.ir.op2] = ALU.leftRotation8(this.mem[this.ir.op2]);
+      break;
+
+      // 41  checkBit (with reg 8 bits) modify z-flag
+      case 41:
+
+      break;
+
+      // 42  checkBit (with mem 8 bits) modify z-flag
+      case 42:
+      break;
+
+      // 43  setBit (with reg 8 bits)
+      case 43:
+      break;
+
+      // 44  setBit (with mem 8 bits)
+      case 44:
+      break;
+
+      // 45  resetBit (with reg 8 bits)
+      case 45:
+      break;
+
+      // 46  resetBit (with mem 8 bits)
+      case 46:
+      break;
+
+      // 47  jump pos(is a memory address)
+      case 47:
+        System.out.println("jump pos");
+        // Supongo que en el operando 2 esta la dir
+        if (this.reg_8bit[A] > 0){
+          // Avanzar PC
+          this.reg_16bit[PC] = this.ir.op2;
+        }
+      break;
+
+      // 48 jump zero
+      case 48:
+        System.out.println("jump zero");
+        if (this.reg_8bit[A] == 0){
+          // Avanzar PC
+          this.reg_16bit[PC] = this.ir.op2;
+        }
+      break;
+
+      // 49 jump
+      case 49:
+        System.out.println("jump");
+      case 50:
+        // Avanzar PC
+        this.reg_16bit[PC] = this.ir.op2;
+      break;
+    }
+  }
+
+
+
+}
